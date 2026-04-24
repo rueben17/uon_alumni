25f3bd4b1967a3eef67b2ba40770efbec6e285b9
rueben17 <rueben17@users.noreply.github.com>
2026-04-24 04:07:59 +0300

    Update project structure

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("2005/", admin.site.urls),
+    
+    # path('user/', include('apps.user.urls', namespace="user")), 
 ]
